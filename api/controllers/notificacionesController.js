46c4179504c72d720c6fec215b26bb2b771c88f6
Nicole <46075568+niicooleperez@users.noreply.github.com>
2022-04-27 16:15:09 -0400

    CHANGE: Se quita ordenamiento por fijadas

--- api/controllers/notificacionesController.js
+++ api/controllers/notificacionesController.js
@@ -5,7 +5,7 @@ exports.getNotificaciones = async (req, res) => {
     
       try {
             const rutPaciente = req.rutPaciente;
-            const notificaciones =  await notificacionPersonalizadas.find({rutPaciente: rutPaciente,deletedAt: null }).sort({ fechaCreacion: -1 }).sort('-fijada').select('-rutPaciente').exec();
+            const notificaciones =  await notificacionPersonalizadas.find({rutPaciente: rutPaciente,deletedAt: null }).sort({ fechaCreacion: -1 }).select('-rutPaciente').exec();
             return res.status(200).send(notificaciones);
       } catch (error) {
       res.status(500).send({ respuesta: await getMensajes("serverError") });
